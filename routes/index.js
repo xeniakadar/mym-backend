f84d511e65a6a2dce79054e7b6beed4d85b00bd9
xeniakadar <xenia.kadar@gmail.com>
2023-10-04 18:33:02 +0200

    google auth fix

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
 const user_controller = require("../controllers/userController");
 const nasa_controller = require("../controllers/nasaController");
 
