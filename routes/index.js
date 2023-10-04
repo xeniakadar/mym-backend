d2655b8b8b7336a0dd0a0c2b224b037bf2cfa63e
xeniakadar <xenia.kadar@gmail.com>
2023-10-04 13:35:32 +0200

    added routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const user_controller = require("../controllers/userController");
+const nasa_controller = require("../controllers/nasaController");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
+
+router.post("/api/login", user_controller.user_login);
+router.post("/api/signup", user_controller.user_signup);
+
+router.get("/api/nasa-daily-image", nasa_controller.get_daily_image);
 
 module.exports = router;
