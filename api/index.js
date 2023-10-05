1403ff08c1cf4fdf770a112739532f4c8454df7a
xeniakadar <xenia.kadar@gmail.com>
2023-10-05 13:41:30 +0200

    google signup

--- api/index.js
+++ api/index.js
@@ -98,6 +98,43 @@ passport.use(
   ),
 );
 
+// email/password
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (err) return done(err);
+        if (!res) {
+          return done(null, false, { message: "Incorrect password" });
+        }
+        return done(null, user);
+      });
+    } catch (err) {
+      return done(err);
+    }
+  }),
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async function (id, done) {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 //////////////
 
 const isLoggedIn = (req, res, next) => {
@@ -154,43 +191,6 @@ app.get("/logout", (req, res) => {
 });
 
 //////////////////
-
-// email/password
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    try {
-      const user = await User.findOne({ username: username });
-      if (!user) {
-        return done(null, false, { message: "Incorrect username" });
-      }
-      bcrypt.compare(password, user.password, (err, res) => {
-        if (err) return done(err);
-        if (!res) {
-          return done(null, false, { message: "Incorrect password" });
-        }
-        return done(null, user);
-      });
-    } catch (err) {
-      return done(err);
-    }
-  }),
-);
-
-passport.serializeUser(function (user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(async function (id, done) {
-  try {
-    const user = await User.findById(id);
-    done(null, user);
-  } catch (err) {
-    done(err);
-  }
-});
-
-app.use(passport.initialize());
-app.use(passport.session());
 
 ////////////////////////////////////////////////////////////////////
 
